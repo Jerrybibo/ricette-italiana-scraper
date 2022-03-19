1ecbbd5d5df32cf07c6e409b504ccc856a089abc
Jerrybibo <59254806+Jerrybibo@users.noreply.github.com>
2022-03-18 21:08:45 -0400

    Finished basic database design and SQL initialization scripts

--- sqlite/drop-tables.sql
+++ sqlite/drop-tables.sql
@@ -2,3 +2,5 @@ DROP TABLE IF EXISTS recipes;
 DROP TABLE IF EXISTS locations;
 DROP TABLE IF EXISTS ingredients;
 DROP TABLE IF EXISTS recipe_ingredients;
+DROP TABLE IF EXISTS preparations;
+DROP TABLE IF EXISTS recipe_preparations;
--- sqlite/init-tables.sql
+++ sqlite/init-tables.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS recipes;
 DROP TABLE IF EXISTS locations;
 DROP TABLE IF EXISTS ingredients;
 DROP TABLE IF EXISTS recipe_ingredients;
+DROP TABLE IF EXISTS preparations;
+DROP TABLE IF EXISTS recipe_preparations;
 
 CREATE TABLE recipes (
     id INTEGER PRIMARY KEY,
@@ -26,5 +28,19 @@ CREATE TABLE recipe_ingredients (
     amount TEXT,
     PRIMARY KEY (ingredient_id, recipe_id),
     FOREIGN KEY (ingredient_id) REFERENCES ingredients(id),
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id)
+);
+
+CREATE TABLE preparations (
+    id INTEGER PRIMARY KEY,
+    term TEXT UNIQUE,
+    definition TEXT
+);
+
+CREATE TABLE recipe_preparations (
+    preparation_id INTEGER,
+    recipe_id INTEGER,
+    PRIMARY KEY (preparation_id, recipe_id),
+    FOREIGN KEY (preparation_id) REFERENCES preparations(id),
     FOREIGN KEY (recipe_id) REFERENCES recipes(id)
 );
